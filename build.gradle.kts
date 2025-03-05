87f8b5d146ea29eed78c046210fb2d219e43381a
Max <max.hertenstein@gmx.de>
2025-03-05 16:23:41 +0100

    pico css

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,12 +15,15 @@ dependencies {
     implementation("io.javalin:javalin-rendering:6.4.0")
     implementation("gg.jte:jte:3.1.16")
     implementation("gg.jte:jte-kotlin:3.1.16")
-
     implementation("org.slf4j:slf4j-simple:2.0.16")
-
     implementation("org.webjars.npm:htmx.org:2.0.4")
-
     implementation("org.webjars.npm:fontsource-variable__jetbrains-mono:5.0.6")
+    implementation("org.webjars.npm:picocss__pico:2.0.6")
+
+
+
+
+
 
 }
 
